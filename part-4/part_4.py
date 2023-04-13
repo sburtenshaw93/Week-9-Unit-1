056dc02a561ac8de795ab710e16b1b82889a8e8b
Sarah Burtenshaw <sarahburtenshaw@Sarahs-MacBook-Air.local>
2023-04-12 22:19:11 -0600

    finished part-4 and started on part-5

--- part-4/part_4.py
+++ part-4/part_4.py
@@ -1,25 +1,75 @@
 ### Step 1 - Input function
 
-## Create five input statements to gather user's book they want to input to the system. After that be sure to turn it into a function.
-
+## Create five input statements to gather user's book they want to input to the system. 
+# After that be sure to turn it into a function.
+print("----------------Step 1--------------------")
 # Code here
-
+# title = input("What book are you looking for?")
+# author = input("What Author are you looking for ")
+# year = input("What is the year of the book was made?")
+# rating = input("Enter rating from 1 - 5")
+# pages = input("Enter pages amount")
+# print(title)
+# print(author)
+# print(year)
+# print(rating)
+# print(pages)
 
 ### Step 2 - Type conversion
 
-## Now convert the proper data-types front strings to either floats or ints depending on what it is. Feel free to comment out your old function so you don't get an error, or copy/paste and give it a new name.
+## Now convert the proper data-types front strings to either floats or ints depending on what it is. 
+# Feel free to comment out your old function so you don't get an error, or copy/paste and give it a new name.
 
 # Code here
-
-
+print("------------------Step 2----------------------")
+# title = input("Add book name here: ")
+# author = input("Name of the author: ")
+# year = int(input("Enter year: "))
+# rating = float(input("Enter rating from 1 - 5: "))
+# pages = int(input("Enter pages count: "))
 
 ### Step 3 - Error handling
 
-## Now take your previous function, and handle potential errors should the user type an answer that doesn't convert data-type properly.
+## Now take your previous function, and handle potential errors 
+# should the user type an answer that doesn't convert data-type properly.
 
 # Code here
+print("-------------------Step 3------------------------")
+def create_book_list():
+    title = input("Add book name here: ")
+    author = input("Name of the author: ")
+    try:
+        year = int(input("Enter year: "))
+    except:
+        year = int(input("Please enter a number: "))
+    try:    
+        rating = float(input("Enter rating from 1 - 5: "))
+    except:
+        rating = float(input("Please enter a number from 1 - 5: "))
+    try:    
+        pages = int(input("Enter pages count: "))
+    except:
+        pages = int(input("Please enter a number: "))
+    
+    dictionary = {
+        "title": title,
+        "author": author,
+        "year": year,
+        "rating": rating,
+        "pages": pages
+    } 
+    return dictionary   
 
-
+def print_library(list):
+    print("\n All listed books \n")
+    for book_library in list:
+        title = book_library["title"]
+        author = book_library["author"]
+        year = book_library["year"]
+        rating = book_library["rating"]
+        pages = book_library["pages"]
+        
+        print(f"Title: {title}, Author: {author}, Year: {year}, Rating: {rating} Pages: {pages}")
 
 ### Step 4 - if/elif/else
 
@@ -27,10 +77,91 @@
 
 # Code here
 
+print("------------------------Step 4-------------------------")
+
+def main_menu(book_information):
+    
+    answer = True
+    
+    user_choices = input("Option 1, Add book: Option 2, View book list: Option 3, Leave page")
+    
+    if user_choices == "1":
+        book_information.append(create_book_list())
+    elif user_choices == "2":
+        print_library(book_information)
+    elif user_choices == "3":
+        print("\n Leaving page \n")
+        answer = False
+    else:
+        print("\n Error: Please enter a number \n")
 
 ### Step 5 - while loops
 
-## Now add a while loop to your main menu to keep it alive, and continually asking for input until the user chooses to exit the program. Call the main menu to ensure it functions properly.
+## Now add a while loop to your main menu to keep it alive,
+# and continually asking for input until the user chooses to exit 
+# the program. Call the main menu to ensure it functions properly.
 
 # Code here
 
+print("-----------------------Step 5---------------------")
+
+library = [
+    {
+        "title": "Fellowship of the ring",
+        "author": "J.R.R Tolkien",
+        "year": "1954",
+        "rating": 4.9,
+        "pages": 423        
+    },
+    {
+        "title": "The Two Towers",
+        "author": "J.R.R Tolkien",
+        "year": "1954", 
+        "rating": 4.5,
+        "pages": 352        
+    },
+    {
+        "title": "The Return of the King",
+        "author": "J.R.R Tolkien",
+        "year": "1955",
+        "rating": 4.9,
+        "pages": 416        
+    },
+    {
+        "title": "The Hobit",
+        "author": "J.R.R Tolkien",
+        "year": "1937",
+        "rating": 4.3,
+        "pages": 304,   
+    },
+    {
+        "title": "Green Eggs and Ham?",
+        "author": "Dr Seuss",
+        "year": "1960",
+        "rating": 4.9,
+        "pages": 50   
+    }
+]
+
+
+def main_menu(book_information):
+    
+    answer = True
+    
+    while answer:
+        user_choices = input("Option 1, Add book: Option 2, View book list: Option 3: Total books in library, Option 4: Total pages, Option 5, Leave page ")
+    
+        if user_choices == "1":
+            book_information.append(create_book_list())
+        elif user_choices == "2":
+            print_library(book_information)
+        elif user_choices == "3":
+            print(f"\n total {len(book_information)} books \n")
+        elif user_choices == "4":
+            print(f"\n Page totals {sum([i['pages'] for i in book_information])} pages \n")
+        elif user_choices == "5":
+            print("\n Leaving page")
+            answer = False
+        else:
+            print("\n Error: Please enter a number")
+main_menu(library)
--- part-5/part_5.py
+++ part-5/part_5.py
@@ -1,25 +1,99 @@
 ### Step 1 - Store data in a .txt
 
-## This step's instructions explains how to use the open() function, to write and read info from a .txt file. Follow the instructions to create and call a function to add a book, based off of the previous dictionaries for our library, to the .txt file properly formatted with commas as separators.
+## This step's instructions explains how to use the open() function, 
+# to write and read info from a .txt file. Follow the instructions to create 
+# and call a function to add a book, based off of the previous dictionaries for our 
+# library, to the .txt file properly formatted with commas as separators.
 
 # Code here
+
+def create_book_list(library):
+    title = input("Add book name here: ")
+    author = input("Name of the author: ")
+    try:
+        year = int(input("Enter year: "))
+    except:
+        year = int(input("Please enter a number: "))
+    try:    
+        rating = float(input("Enter rating from 1 - 5: "))
+    except:
+        rating = float(input("Please enter a number from 1 - 5: "))
+    try:    
+        pages = int(input("Enter pages count: "))
+    except:
+        pages = int(input("Please enter a number: "))
+    with open(library, "i") as file:
+        file.write(f"{title}, {author}, {year}, {rating}, {pages}\n")    
 
 
 ### Step 2 - Read data from a .txt
 
-## Now take your previously create function which prints info about all the books in your library, but gets the info from a list, and make it work by reading the information in your home library's .txt document. This will take some new logic, but you can do it.
+## Now take your previously create function which prints info about all
+# the books in your library, but gets the info from a list, and make it work
+# by reading the information in your home library's .txt document. This will take some new logic, but you can do it.
 
 # Code here
 
+def print_library(book):
+    print("\n All books listed\n")
+    
+    with open(book, "r") as file:
+        lines = file.readlines()
+        
+        for line in lines:
+            title, author, year, rating, pages = line.strip().split(",")
+            print(f"Title: {title}, Author: {author}, Year: {year}, Rating: {rating}, Pages: {pages}")
 
 ### Step 3 - if __name__ == "__main__":
 
-## Wrap your main menu function call in an "if __name__ == '__main__':" statement to ensure it doesn't accidentally run if this file is imported as a module elsewhere.
+## Wrap your main menu function call in an "if __name__ == '__main__':" statement
+# to ensure it doesn't accidentally run if this file is imported as a module elsewhere.
 
 # Code this at the bottom of the script
 
 
 ### Step 4 - Expand and refactor
 
-## Now follow the instructions in this final step. Expand your project. Clean up the code. Make your application functional. Great job getting your first Python application finished!
+## Now follow the instructions in this final step. Expand your project. 
+# Clean up the code. Make your application functional. Great job getting your first Python application finished!
 
+def main_menu(book_source):
+
+    active = True
+
+    while active:
+        choice = input("""
+Choose 1 to add a book...
+Choose 2 to see all your books...
+Choose 3 to see a count of your books...
+Choose 4 to see a count of the pages of your books...
+Choose 5 to see the your highest rated book...
+Choose 6 to see your lowest rated book...
+Choose 7 to see your books ranked by order of rating...
+Choose 0 to exit.
+Type here: """)
+
+        if choice == "1":
+            create_new_book(book_source)
+        elif choice == "2":
+            view_books(book_source)
+        elif choice == "3":
+            print(f"\nYou have a total of {len(get_books_as_list_of_dictionaries(book_source))} books.\n")
+        elif choice == "4":
+            print(f"\nYou books page count totals {sum([x['pages'] for x in get_books_as_list_of_dictionaries(book_source)])} pages!\n")
+        elif choice == "5":
+            print("\nHere is your highest rated book...\n")
+            get_book_printed(get_highest_rated_book(book_source))
+        elif choice == "6":
+            print("\nHere is your lowest rated book...\n")
+            get_book_printed(get_lowest_rated_book(book_source))
+        elif choice == "7":
+            get_sorted_list_by_rating(book_source)
+        elif choice == "0":
+            print("\nGoodbye")
+            active = False
+        else:
+            print("\nSorry please type a number for one of the options.\n")
+
+if __name__ == "__main__":
+    main_menu("library.txt")
