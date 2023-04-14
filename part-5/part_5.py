c8daa49503f1de206a6f177232dfa5eab58b147f
Sarah Burtenshaw <sarahburtenshaw@Sarahs-MacBook-Air.local>
2023-04-13 20:32:23 -0600

    Finsihed part-5 ready to commit

--- part-5/part_5.py
+++ part-5/part_5.py
@@ -1,9 +1,52 @@
+print(f"The dunder name of part_5.py is {__name__}")
+
+library = [
+    {
+        "title": "Fellowship of the ring",
+        "author": "J.R.R Tolkien",
+        "year": "1954",
+        "rating": 4.9,
+        "pages": 423        
+    },
+    {
+        "title": "The Two Towers",
+        "author": "J.R.R Tolkien",
+        "year": "1954", 
+        "rating": 4.5,
+        "pages": 352        
+    },
+    {
+        "title": "The Return of the King",
+        "author": "J.R.R Tolkien",
+        "year": "1955",
+        "rating": 4.9,
+        "pages": 416        
+    },
+    {
+        "title": "The Hobit",
+        "author": "J.R.R Tolkien",
+        "year": "1937",
+        "rating": 4.3,
+        "pages": 304,   
+    },
+    {
+        "title": "Green Eggs and Ham?",
+        "author": "Dr Seuss",
+        "year": "1960",
+        "rating": 4.9,
+        "pages": 50   
+    }
+]
+
 ### Step 1 - Store data in a .txt
 
-## This step's instructions explains how to use the open() function, 
-# to write and read info from a .txt file. Follow the instructions to create 
-# and call a function to add a book, based off of the previous dictionaries for our 
-# library, to the .txt file properly formatted with commas as separators.
+## This step's instructions explains how to use the open()function, 
+# to write and read info from a .txt file.
+# Follow the instructions to create 
+# and call a function to add a book, 
+# based off of the previous dictionaries for our 
+# library, to the .txt file properly 
+# formatted with commas as separators.
 
 # Code here
 
@@ -22,7 +65,7 @@ def create_book_list(library):
         pages = int(input("Enter pages count: "))
     except:
         pages = int(input("Please enter a number: "))
-    with open(library, "i") as file:
+    with open(library, "a") as file:
         file.write(f"{title}, {author}, {year}, {rating}, {pages}\n")    
 
 
@@ -42,58 +85,62 @@ def print_library(book):
         
         for line in lines:
             title, author, year, rating, pages = line.strip().split(",")
-            print(f"Title: {title}, Author: {author}, Year: {year}, Rating: {rating}, Pages: {pages}")
+            book_dictionary = {
+                "Title": title,
+                "Author": author,
+                "Year": year,
+                "Rating": float(rating),
+                "Pages": int(pages)
+            }
+            print(book_dictionary)
+            
+            
 
 ### Step 3 - if __name__ == "__main__":
 
-## Wrap your main menu function call in an "if __name__ == '__main__':" statement
-# to ensure it doesn't accidentally run if this file is imported as a module elsewhere.
+## Wrap your main menu function call in an 
+# "if __name__ == '__main__':" statement
+# to ensure it doesn't accidentally run if 
+# this file is imported as a module elsewhere.
 
 # Code this at the bottom of the script
 
 
 ### Step 4 - Expand and refactor
 
-## Now follow the instructions in this final step. Expand your project. 
-# Clean up the code. Make your application functional. Great job getting your first Python application finished!
+## Now follow the instructions in this final step. 
+# Expand your project. 
+# Clean up the code. Make your application functional.
+# Great job getting your first Python application finished!
 
-def main_menu(book_source):
-
-    active = True
-
-    while active:
-        choice = input("""
-Choose 1 to add a book...
-Choose 2 to see all your books...
-Choose 3 to see a count of your books...
-Choose 4 to see a count of the pages of your books...
-Choose 5 to see the your highest rated book...
-Choose 6 to see your lowest rated book...
-Choose 7 to see your books ranked by order of rating...
-Choose 0 to exit.
-Type here: """)
-
-        if choice == "1":
-            create_new_book(book_source)
-        elif choice == "2":
-            view_books(book_source)
-        elif choice == "3":
-            print(f"\nYou have a total of {len(get_books_as_list_of_dictionaries(book_source))} books.\n")
-        elif choice == "4":
-            print(f"\nYou books page count totals {sum([x['pages'] for x in get_books_as_list_of_dictionaries(book_source)])} pages!\n")
-        elif choice == "5":
-            print("\nHere is your highest rated book...\n")
-            get_book_printed(get_highest_rated_book(book_source))
-        elif choice == "6":
-            print("\nHere is your lowest rated book...\n")
-            get_book_printed(get_lowest_rated_book(book_source))
-        elif choice == "7":
-            get_sorted_list_by_rating(book_source)
-        elif choice == "0":
-            print("\nGoodbye")
-            active = False
+def main_menu(book_information):
+    
+    answer = True
+    
+    while answer:
+        user_choices = input("Option 1, Add book: Option 2, View book list: Option 3: Total books in library, Option 4: Total pages, Option 5, Leave page ")
+    
+        if user_choices == "1":
+            create_book_list(book_information)
+        elif user_choices == "2":
+            print_library(book_information)
+        elif user_choices == "3":
+             with open(book_information, "r") as file:
+                lines = file.readlines()
+                print(f"\n total {len(lines)} books \n")
+        elif user_choices == "4":
+            with open(book_information, "r") as file:
+                lines = file.readlines()
+                total_pages = 0
+                for line in lines:
+                    title, author, year, rating, pages = line.strip().split(",")
+                    total_pages = total_pages + int(pages)
+                print(f"\n Page totals {total_pages} pages \n")
+        elif user_choices == "5":
+            print("\n Leaving page")
+            answer = False
         else:
-            print("\nSorry please type a number for one of the options.\n")
+            print("\n Error: Please enter a number")
 
-if __name__ == "__main__":
-    main_menu("library.txt")
+if __name__ == '__main__':
+    main_menu('test.txt')
